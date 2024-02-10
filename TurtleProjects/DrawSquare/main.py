0ee200214d25e03936ed3416b1cace19b26bec31
Ferdi Can <fcan5414@gmail.com>
2024-02-10 21:52:53 +0300

    Draw square finished

---
+++ TurtleProjects/DrawSquare/main.py
@@ -0,0 +1,12 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("aquamarine1")
+screen.title("Draw Square")
+
+turtleInstance = turtle.Turtle()
+
+for i in range(4):
+    turtleInstance.forward(200)
+    turtleInstance.left(90)
+turtle.done()
