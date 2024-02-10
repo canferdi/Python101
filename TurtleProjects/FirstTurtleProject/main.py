83cc8839ef3328565b05a9d0c5e4bfb64d55112b
Ferdi Can <fcan5414@gmail.com>
2024-02-10 20:52:24 +0300

    First turtle project finished

---
+++ TurtleProjects/FirstTurtleProject/main.py
@@ -0,0 +1,11 @@
+import turtle
+
+drawingBoard = turtle.Screen()
+drawingBoard.bgcolor("green")
+drawingBoard.title("First Turtle Project")
+turtleInstance = turtle.Turtle()
+
+turtleInstance.forward(100)
+turtleInstance.left(90)
+turtleInstance.forward(100)
+turtle.done()
